e5df777852c9625de4f6fe30b9d3ab8a52d93a6a
Lang Martin <lang@hashicorp.com>
2020-01-20 17:06:57 -0500

    api: CSI plugin queries, ControllerHealthy -> ControllersHealthy

--- api/csi_test.go
+++ api/csi_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestCSIVolumes_CRUD(t *testing.T) {
@@ -32,14 +33,16 @@ func TestCSIVolumes_CRUD(t *testing.T) {
 	}
 
 	// Register a volume
-	v.Register(&CSIVolume{
-		ID:             "DEADBEEF-63C7-407F-AE82-C99FBEF78FEB",
-		Driver:         "minnie",
+	id := "DEADBEEF-31B5-8F78-7986-DD404FDA0CD1"
+	err = v.Register(&CSIVolume{
+		ID:             id,
 		Namespace:      "default",
+		PluginID:       "adam",
 		AccessMode:     CSIVolumeAccessModeMultiNodeSingleWriter,
 		AttachmentMode: CSIVolumeAttachmentModeFilesystem,
 		Topologies:     []*CSITopology{{Segments: map[string]string{"foo": "bar"}}},
 	}, wpts)
+	assert.NoError(t, err)
 
 	// Successful result with volumes
 	vols, qm, err = v.List(opts)
@@ -48,13 +51,12 @@ func TestCSIVolumes_CRUD(t *testing.T) {
 	assert.Equal(t, 1, len(vols))
 
 	// Successful info query
-	vol, qm, err := v.Info("DEADBEEF-63C7-407F-AE82-C99FBEF78FEB", opts)
+	vol, qm, err := v.Info(id, opts)
 	assert.NoError(t, err)
-	assert.Equal(t, "minnie", vol.Driver)
 	assert.Equal(t, "bar", vol.Topologies[0].Segments["foo"])
 
 	// Deregister the volume
-	err = v.Deregister("DEADBEEF-63C7-407F-AE82-C99FBEF78FEB", wpts)
+	err = v.Deregister(id, wpts)
 	assert.NoError(t, err)
 
 	// Successful empty result
@@ -64,6 +66,55 @@ func TestCSIVolumes_CRUD(t *testing.T) {
 	assert.Equal(t, 0, len(vols))
 
 	// Failed info query
-	vol, qm, err = v.Info("DEADBEEF-63C7-407F-AE82-C99FBEF78FEB", opts)
+	vol, qm, err = v.Info(id, opts)
 	assert.Error(t, err, "missing")
+}
+
+func TestCSIPlugins_viaJob(t *testing.T) {
+	t.Parallel()
+	c, s, root := makeACLClient(t, nil, nil)
+	defer s.Stop()
+	p := c.CSIPlugins()
+
+	// Successful empty result
+	plugs, qm, err := p.List(nil)
+	assert.NoError(t, err)
+	assert.NotEqual(t, 0, qm.LastIndex)
+	assert.Equal(t, 0, len(plugs))
+
+	// Authorized QueryOpts. Use the root token to just bypass ACL details
+	opts := &QueryOptions{
+		Region:    "global",
+		Namespace: "default",
+		AuthToken: root.SecretID,
+	}
+
+	wpts := &WriteOptions{
+		Region:    "global",
+		Namespace: "default",
+		AuthToken: root.SecretID,
+	}
+
+	// Register a plugin job
+	j := c.Jobs()
+	job := testJob()
+	job.Namespace = stringToPtr("default")
+	job.TaskGroups[0].Tasks[0].CSIPluginConfig = &TaskCSIPluginConfig{
+		ID:       "foo",
+		Type:     "monolith",
+		MountDir: "/not-empty",
+	}
+	_, _, err = j.Register(job, wpts)
+	require.NoError(t, err)
+
+	// Successful result with the plugin
+	plugs, qm, err = p.List(opts)
+	assert.NoError(t, err)
+	assert.NotEqual(t, 0, qm.LastIndex)
+	assert.Equal(t, 1, len(plugs))
+
+	// Successful info query
+	plug, qm, err := p.Info("foo", opts)
+	require.NoError(t, err)
+	assert.Equal(t, *job.ID, *plug.Jobs[*job.Namespace][*job.ID].ID)
 }
